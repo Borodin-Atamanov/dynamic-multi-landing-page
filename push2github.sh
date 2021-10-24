f0b1e239301bf403b7adacfe5fa8aab957728a2d
Borodin-Atamanov <github@Borodin-Atamanov.ru>
2021-10-24 22:21:25 +0400

    Two methods of change replacements

--- push2github.sh
+++ push2github.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 set -x;
-time (echo git pull --verbose; git add --verbose --all; git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --verbose;)
+#time (echo git pull --verbose; git add --verbose --all; git commit --allow-empty-message --message=$(date "+%F_%H-%M-%S") --verbose; git push --verbose;)
+time (echo git pull --verbose; git add --verbose --all; git commit --allow-empty-message --verbose; git push --verbose;)
